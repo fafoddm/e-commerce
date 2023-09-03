5dc0f726ca2bf0525395aff5970bcc9d6be95a1f
fafoddm <fabriddm@gmail.com>
2023-09-03 19:40:33 -0300

    entrega 2

--- js/categories.js
+++ js/categories.js
@@ -49,26 +49,22 @@ function showCategoriesList(){
         if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
             ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
 
-            htmlContentToAppend += `
-            <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${category.name}</h4>
-                            <small class="text-muted">${category.productCount} artículos</small>
-                        </div>
-                        <p class="mb-1">${category.description}</p>
-                    </div>
-                </div>
-            </div>
-            `
-        }
-
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
-    }
+                htmlContentToAppend += `
+                <div onclick="setCatID(${category.id})"  class="col" id="container" >
+                   <div class="card" style="width: 18rem;" style="border: 5px black;" style=":hover: ">
+                       <img src="${category.imgSrc}" alt="${category.description}" class="card-img-top">
+                       <div class="card-body">
+                               <h4>${category.name}</h4>
+                               <small class="card-text">${category.productCount} artículos</small>
+                           <p>${category.description}</p>
+                       </div>
+                   </div>
+               </div>
+               `
+           }
+   
+           document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+       }
 }
 
 function sortAndShowCategories(sortCriteria, categoriesArray){
